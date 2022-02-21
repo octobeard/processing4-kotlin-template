4bc27a9373445e15b4e820ef6845519d38fc2d1e
Mark Fleschler <mark.f.accts@gmail.com>
2022-02-21 14:34:50 -0800

    Removed local processing libs; added jitpack maven dep

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,26 @@
+import java.net.URI
+
 plugins {
     application
     kotlin("jvm") version "1.6.10"
 }
 
-group = "org.example"
+group = "com.octobeard"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = URI("https://jitpack.io") }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+    // import processing4 beta 6 library via jitpack maven mirror
+    implementation("com.github.micycle1:processing-core-4:4.0b6c")
     // import HYPE jar in libs directory 2.x
     implementation(fileTree(mapOf("dir" to "libs/hype", "include" to listOf("*.jar"))))
     // import minim library 2.2.2
     implementation(fileTree(mapOf("dir" to "libs/minim", "include" to listOf("*.jar"))))
-    // import processing4 beta 6 library
-    implementation(fileTree(mapOf("dir" to "libs/processing4", "include" to listOf("*.jar"))))
 }
 
 application {
